87c786382f6bea4044ef7f827ef18425f07762ab
Nicolas Da Mutten <nicolas@damutten.ch>
2023-08-06 20:22:27 +0200

    WIP: use different lib

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from smartgadget import SHT40, AnyDeviceManager
+from sensirionbt import SmartGadget
 
 
 def test_callback_temp(temp: int) -> None:
@@ -10,36 +10,8 @@ def test_callback_humid(temp: int) -> None:
 
 
 def run():
-    manager = AnyDeviceManager(adapter_name="hci0")
-    manager.start_discovery(service_uuids=[SHT40.t_uuid, SHT40.h_uuid])
-    print("Started Discovery")
-
-    bedroom = SHT40(
-        mac_address="",
-        manager=manager,
-        temperature_callback=test_callback_temp,
-        humidity_callback=test_callback_humid,
-    )
-
-    bedroom.connect()
-
-    print("bedroom connected. Stopping discovery")
-
-    # all devices connected, so stop discovering
-    manager.stop_discovery()
-
-    print("Discovery Stopped. Starting run loop")
-
-    # RunLoop
-    try:
-        manager.run()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        if callable(bedroom.disconnect):
-            print("disconnecting")
-            bedroom.disconnect()
-            print("disconnected")
+    bedroom = SmartGadget("FC:CE:02:A5:24:A7")
+    bedroom.set
 
 
 # Press the green button in the gutter to run the script.
